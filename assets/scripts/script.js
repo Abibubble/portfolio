a13f56d770f6d1bc6a8898918424f181b7ab4017
Abibubble <abi.yamaha.neo@gmail.com>
2021-01-27 19:02:23 +0000

    maint: Remove unnecessary code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,62 +1,6 @@
-function onehover(){
+function hover(){
 	document.getElementById("skillone").style.fontSize = "200%";
 }
-function onenormal() {
+function normal() {
 	document.getElementById("skillone").style.fontSize = "100%";
-}
-
-function twohover(){
-	document.getElementById("skilltwo").style.fontSize = "200%";
-}
-function twonormal() {
-	document.getElementById("skilltwo").style.fontSize = "100%";
-}
-
-function threehover(){
-	document.getElementById("skillthree").style.fontSize = "200%";
-}
-function threenormal() {
-	document.getElementById("skillthree").style.fontSize = "100%";
-}
-
-function fourhover(){
-	document.getElementById("skillfour").style.fontSize = "200%";
-}
-function fournormal() {
-	document.getElementById("skillfour").style.fontSize = "100%";
-}
-
-function fivehover(){
-	document.getElementById("skillfive").style.fontSize = "200%";
-}
-function fivenormal() {
-	document.getElementById("skillfive").style.fontSize = "100%";
-}
-
-function sixhover(){
-	document.getElementById("skillsix").style.fontSize = "200%";
-}
-function sixnormal() {
-	document.getElementById("skillsix").style.fontSize = "100%";
-}
-
-function sevenhover(){
-	document.getElementById("skillseven").style.fontSize = "200%";
-}
-function sevennormal() {
-	document.getElementById("skillseven").style.fontSize = "100%";
-}
-
-function eighthover(){
-	document.getElementById("skilleight").style.fontSize = "200%";
-}
-function eightnormal() {
-	document.getElementById("skilleight").style.fontSize = "100%";
-}
-
-function ninehover(){
-	document.getElementById("skillnine").style.fontSize = "200%";
-}
-function ninenormal() {
-	document.getElementById("skillnine").style.fontSize = "100%";
 }
