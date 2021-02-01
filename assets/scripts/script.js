363d33fc6b085b04f2892d8e913e08fcb3c4ed7c
Abibubble <abi.yamaha.neo@gmail.com>
2021-02-01 12:49:18 +0000

    wip: Add to dropdown navbar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,11 +6,11 @@ function normal(id) {
 }
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+function dropdownMenuToggle() {
+    let navbarContainer = document.getElementById("navbar-container");
+    if (navbarContainer.classList.contains("hide")) {
+        navbarContainer.classList.remove("hide");
+    } else {
+        navbarContainer.classList.add("hide");
+    }
 }
